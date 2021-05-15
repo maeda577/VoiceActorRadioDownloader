e1950c0c92cddf412ff81c1400c5875df8c063f3
maeda577 <kazu55632000@gmail.com>
2021-05-15 22:28:43 +0900

    引数でのパラメータ指定を廃止

--- start.ps1
+++ start.ps1
@@ -1,61 +1,25 @@
 ﻿[CmdletBinding()]
 param (
-    [Parameter(ParameterSetName = "Arguments")]
-    [String[]]
-    $HibikiAccessIds = @(),
-
-    [Parameter(ParameterSetName = "Arguments")]
-    [String[]]
-    $OnsenDirectoryNames = @(),
-
-    [Parameter(ParameterSetName = "Arguments", Mandatory=$true)]
+    [Parameter(Mandatory=$true)]
+    [String]
     [ValidateScript({Test-Path $_})]
-    [String]
-    $DestinationPath,
-
-    [Parameter(ParameterSetName = "Arguments")]
-    [String]
-    $PodcastBaseUrl = "http://localhost/",
-
-    [Parameter(ParameterSetName = "Arguments")]
-    [String]
-    $FfmpegPath = "ffmpeg",
-
-    [Parameter(ParameterSetName = "Arguments")]
-    [String]
-    $FfprobePath = "ffprobe",
-
-    [Parameter(ParameterSetName = "ConfigFile", Mandatory=$true)]
-    [String]
     $ConfigurationFilePath
 )
 
-# Ubuntu環境だとカンマ区切りの引数を指定しても自動で分割されなかったので切る
-if ($HibikiAccessIds.Length -eq 1) {
-    $HibikiAccessIds = $HibikiAccessIds.Split(",") | ForEach-Object { $_.Trim() }
-}
-if ($OnsenDirectoryNames.Length -eq 1) {
-    $OnsenDirectoryNames = $OnsenDirectoryNames.Split(",") | ForEach-Object { $_.Trim() }
+# config読み取り
+$config = Get-Content -Path $ConfigurationFilePath -Encoding UTF8 | ConvertFrom-Json
+
+if ((Test-Path $config.DestinationPath) -eq $false) {
+    throw "DestinationPath is not exists"
 }
 
-# configファイルが指定されていた場合
-if ($ConfigurationFilePath) {
-    $config = Get-Content -Path $ConfigurationFilePath -Encoding UTF8 | ConvertFrom-Json
-    $HibikiAccessIds = $config.Hibiki.AccessIds
-    $OnsenDirectoryNames = $config.Onsen.DirectoryNames
-    $OnsenEmail = $config.Onsen.Email
-    $OnsenPassword = $config.Onsen.Password
-    $DestinationPath = $config.DestinationPath
-    $PodcastBaseUrl = $config.PodcastBaseUrl
-    $FfmpegPath = $config.Ffmpeg.FfmpegPath
-    $FfprobePath = $config.Ffmpeg.FfprobePath
-}
-
+$FfmpegPath = $config.Ffmpeg.FfmpegPath
 if ($FfmpegPath -eq "") {
-    $FfmpegPath = "ffmpeg"
+    $FfmpegPath = $null
 }
+$FfprobePath = $config.Ffmpeg.FfprobePath
 if ($FfprobePath -eq "") {
-    $FfprobePath = "ffprobe"
+    $FfprobePath = $null
 }
 
 Import-Module -Force $PSScriptRoot/SaveHibikiRadio.psm1
@@ -63,18 +27,18 @@ Import-Module -Force $PSScriptRoot/SaveOnsenRadio.psm1
 Import-Module -Force $PSScriptRoot/UpdatePodcastFeed.psm1
 
 # Hibikiのダウンロードとrss生成
-if ($HibikiAccessIds) {
-    $HibikiAccessIds | Save-HibikiRadio -DestinationPath $DestinationPath -FfmpegPath $FfmpegPath
-    $HibikiAccessIds | Update-HibikiRadioFeed -DestinationPath $DestinationPath -PodcastBaseUrl $PodcastBaseUrl -FfprobePath $FfprobePath
+if ($config.Hibiki.AccessIds) {
+    $config.Hibiki.AccessIds | Save-HibikiRadio -DestinationPath $config.DestinationPath -FfmpegPath $FfmpegPath
+    $config.Hibiki.AccessIds | Update-HibikiRadioFeed -DestinationPath $config.DestinationPath -PodcastBaseUrl $config.PodcastBaseUrl -FfprobePath $FfprobePath
 }
 
 # 音泉のダウンロードとrss生成
-if($OnsenEmail -and $OnsenPassword){
-    $session = Connect-OnsenPremium -Email $OnsenEmail -Password $OnsenPassword
+if($config.Onsen.Email -and $config.Onsen.Password){
+    $session = Connect-OnsenPremium -Email $config.Onsen.Email -Password $config.Onsen.Password
 }
-if ($nsenDirectoryNames) {
-    $OnsenDirectoryNames | Save-OnsenRadio -DestinationPath $DestinationPath -Session $session -FfmpegPath $FfmpegPath
-    $OnsenDirectoryNames | Update-OnsenRadioFeed -DestinationPath $DestinationPath -PodcastBaseUrl $PodcastBaseUrl -FfprobePath $FfprobePath
+if ($config.Onsen.DirectoryNames) {
+    $config.Onsen.DirectoryNames | Save-OnsenRadio -DestinationPath $config.DestinationPath -Session $session -FfmpegPath $FfmpegPath
+    $config.Onsen.DirectoryNames | Update-OnsenRadioFeed -DestinationPath $config.DestinationPath -PodcastBaseUrl $config.PodcastBaseUrl -FfprobePath $FfprobePath
 }
 if ($session) {
     Disconnect-OnsenPremium -Session $session
