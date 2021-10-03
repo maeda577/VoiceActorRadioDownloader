462a06798fa1e53fdb00ea62302b62a2238bf217
maeda577 <kazu55632000@gmail.com>
2021-10-03 11:31:42 +0900

    ディレクトリ構成を変更

---
+++ modules/VoiceActorRadioDownloader.psm1
@@ -0,0 +1,63 @@
+﻿function Start-VoiceActorRadioDownloader {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory = $true)]
+        [String]
+        [ValidateScript( { Test-Path $_ })]
+        $ConfigurationFilePath
+    )
+
+    # config読み取り
+    $config = Get-Content -Path $ConfigurationFilePath -Encoding UTF8 | ConvertFrom-Json
+
+    if ((Test-Path $config.DestinationPath) -eq $false) {
+        throw "DestinationPath is not exists"
+    }
+
+    $FfmpegPath = $config.Ffmpeg.FfmpegPath
+    if ([System.String]::IsNullOrWhiteSpace($FfmpegPath)) {
+        $FfmpegPath = "ffmpeg"
+    }
+    $FfprobePath = $config.Ffmpeg.FfprobePath
+    if ([System.String]::IsNullOrWhiteSpace($FfprobePath)) {
+        $FfprobePath = "ffprobe"
+    }
+
+    # Hibikiのダウンロード
+    if ($config.Hibiki.AccessIds) {
+        $config.Hibiki.AccessIds | Save-HibikiRadio -DestinationPath $config.DestinationPath -FfmpegPath $FfmpegPath
+    }
+
+    # 音泉のダウンロード
+    if ($config.Onsen.Email -and $config.Onsen.Password) {
+        $session = Connect-OnsenPremium -Email $config.Onsen.Email -Password $config.Onsen.Password
+    }
+    if ($config.Onsen.DirectoryNames) {
+        $config.Onsen.DirectoryNames | Save-OnsenRadio -DestinationPath $config.DestinationPath -Session $session -FfmpegPath $FfmpegPath
+    }
+    if ($session) {
+        Disconnect-OnsenPremium -Session $session
+    }
+
+    # Radikoタイムフリーのダウンロード
+    if ($config.Radiko.Programs) {
+        $radiko = Connect-Radiko
+        foreach ($program in $config.Radiko.Programs) {
+            # 視聴エリア外の場合はスキップ
+            if ($program.StationId -notin $radiko.StationIds) {
+                Write-Warning -Message "Current area $($radiko.AreaId) can't listen station $($program.StationId). Skipping."
+                Continue
+            }
+
+            Save-Radiko -AuthToken $radiko.AuthToken -StationId $program.StationId -MatchTitle $program.MatchTitle -DestinationPath $config.DestinationPath -DestinationSubDir $program.LocalDirectoryName -Session $session -FfmpegPath $FfmpegPath
+        }
+    }
+
+    # RadioTalkのダウンロード
+    if ($config.RadioTalk.ProgramIds) {
+        $config.RadioTalk.ProgramIds | Save-RadioTalk -DestinationPath $config.DestinationPath -FfmpegPath $FfmpegPath 6>&1
+    }
+
+    # Podcast用RSS更新
+    Get-ChildItem -Path $config.DestinationPath -Filter "info.json" -File -Recurse | Update-RadioFeed -DestinationPath $config.DestinationPath -PodcastBaseUrl $config.PodcastBaseUrl -FfprobePath $FfprobePath
+}
--- start.ps1
+++ start.ps1
@@ -6,65 +6,7 @@ param (
     $ConfigurationFilePath
 )
 
-$ErrorActionPreference = "Stop"
+Get-ChildItem -Path $PSScriptRoot/modules/ -Filter *.psm1 |
+    ForEach-Object -Process { Import-Module -Force -Name $_.FullName }
 
-# config読み取り
-$config = Get-Content -Path $ConfigurationFilePath -Encoding UTF8 | ConvertFrom-Json
-
-if ((Test-Path $config.DestinationPath) -eq $false) {
-    throw "DestinationPath is not exists"
-}
-
-$FfmpegPath = $config.Ffmpeg.FfmpegPath
-if ([System.String]::IsNullOrWhiteSpace($FfmpegPath)) {
-    $FfmpegPath = "ffmpeg"
-}
-$FfprobePath = $config.Ffmpeg.FfprobePath
-if ([System.String]::IsNullOrWhiteSpace($FfprobePath)) {
-    $FfprobePath = "ffprobe"
-}
-
-Import-Module -Force $PSScriptRoot/common.psm1
-Import-Module -Force $PSScriptRoot/SaveHibikiRadio.psm1
-Import-Module -Force $PSScriptRoot/SaveOnsenRadio.psm1
-Import-Module -Force $PSScriptRoot/SaveRadiko.psm1
-Import-Module -Force $PSScriptRoot/SaveRadioTalk.psm1
-Import-Module -Force $PSScriptRoot/UpdatePodcastFeed.psm1
-
-# Hibikiのダウンロード
-if ($config.Hibiki.AccessIds) {
-    $config.Hibiki.AccessIds | Save-HibikiRadio -DestinationPath $config.DestinationPath -FfmpegPath $FfmpegPath
-}
-
-# 音泉のダウンロード
-if ($config.Onsen.Email -and $config.Onsen.Password) {
-    $session = Connect-OnsenPremium -Email $config.Onsen.Email -Password $config.Onsen.Password
-}
-if ($config.Onsen.DirectoryNames) {
-    $config.Onsen.DirectoryNames | Save-OnsenRadio -DestinationPath $config.DestinationPath -Session $session -FfmpegPath $FfmpegPath
-}
-if ($session) {
-    Disconnect-OnsenPremium -Session $session
-}
-
-# Radikoタイムフリーのダウンロード
-if ($config.Radiko.Programs) {
-    $radiko = Connect-Radiko
-    foreach ($program in $config.Radiko.Programs) {
-        # 視聴エリア外の場合はスキップ
-        if ($program.StationId -notin $radiko.StationIds) {
-            Write-Warning -Message "Current area $($radiko.AreaId) can't listen station $($program.StationId). Skipping."
-            Continue
-        }
-
-        Save-Radiko -AuthToken $radiko.AuthToken -StationId $program.StationId -MatchTitle $program.MatchTitle -DestinationPath $config.DestinationPath -DestinationSubDir $program.LocalDirectoryName -Session $session -FfmpegPath $FfmpegPath
-    }
-}
-
-# RadioTalkのダウンロード
-if ($config.RadioTalk.ProgramIds) {
-    $config.RadioTalk.ProgramIds | Save-RadioTalk -DestinationPath $config.DestinationPath -FfmpegPath $FfmpegPath 6>&1
-}
-
-# Podcast用RSS更新
-Get-ChildItem -Path $config.DestinationPath -Filter "info.json" -File -Recurse | Update-RadioFeed -DestinationPath $config.DestinationPath -PodcastBaseUrl $config.PodcastBaseUrl -FfprobePath $FfprobePath
+Start-VoiceActorRadioDownloader -ConfigurationFilePath $ConfigurationFilePath -ErrorAction Stop -WhatIf:$WhatIfPreference
